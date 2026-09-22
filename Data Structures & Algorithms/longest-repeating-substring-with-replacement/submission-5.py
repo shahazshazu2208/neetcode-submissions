e2847a4ed6a2803a9146e8e10853c91b723074d6
shahazshazu2208 <shahazshazu2208@gmail.com>
2026-09-22 20:15:48 +0530

    Add: longest-repeating-substring-with-replacement - submission-5

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-5.py
@@ -0,0 +1,20 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        cset=set()
+        l=0
+        mlen=0
+        for r in range(len(s)):
+            while s[r] in cset:
+                cset.remove(s[l])
+                l += 1
+            cset.add(s[r])
+            mlen=max(mlen,r-l+1)
+        return mlen
+
+
+
+
+
+
+
+        
